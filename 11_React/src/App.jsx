8deafb6c5e0a5ed72c11f35361c911ce9a2ffc6c
sourish19 <sourish19z@gmail.com>
2025-07-05 22:24:19 +0000

    Initialized Accordian

---
+++ 11_React/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+const App = () => {
+  return (
+    <div className="bg-gray-950 min-h-screen flex justify-center items-center">
+      <div className="text-white w-[50%] min-h-[500px] bg-amber-100 ">
+        Hello
+      </div>
+    </div>
+  );
+};
+
+export default App;
