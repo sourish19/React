583e056eaa32cd1774625f32d82658c9e850f868
sourish19 <sourish19z@gmail.com>
2025-07-18 18:18:41 +0000

    Search bttn added

--- 15_React/src/components/Sidebar.jsx
+++ 15_React/src/components/Sidebar.jsx
@@ -1,9 +1,16 @@
 const Sidebar = () => {
   return (
-    <div class="w-20 bg-neutral-800 hover:w-64 transition-all duration-300 h-screen">
-      <div className="flex flex-col justify-center items-center">
-        <span className="bi bi-list text-xl mt-5"></span>
-        <span class="bi bi-pencil-square text-lg mt-10 opacity-30"></span>
+    <div className="group relative flex flex-col items-start w-20 h-screen px-6 py-8 bg-neutral-800 hover:w-64 transition-all duration-300 overflow-hidden">
+      <div className="flex justify-between w-full mt-6">
+        <div>
+          <i className="bi bi-list text-xl w-10 rounded-full hover:bg-amber-50" />
+        </div>
+        <div className="absolute origin-right left-52 mt-1 scale-0 group-hover:scale-100 transition-transform duration-500">
+          <i className="bi bi-search"></i>
+        </div>
+      </div>
+      <div className="mt-10 w-5 text-lg hover:bg-amber-50 opacity-30">
+        <i className="bi bi-pencil-square" />
       </div>
     </div>
   );
